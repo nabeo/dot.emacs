50b96162ba947304b51bdcd19511a50c0aff8bf7
nabeo <watanabe.michikazu@gmail.com>
2015-11-22 21:24:19 +0900

    update

--- cocoa/my-customize.el
+++ cocoa/my-customize.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (helm-swoop yafolding twittering-mode ruby-end ruby-electric ruby-compilation ruby-additional projectile-codesearch popwin pallet neotree markdown-mode magit-svn magit-find-file id-manager helm-rb helm-projectile helm-package helm-migemo helm-ls-svn helm-ls-hg helm-ls-git helm-gtags helm-go-package helm-gitignore helm-git-grep helm-git-files helm-git helm-gist helm-flycheck helm-ag go-projectile ac-js2 ac-inf-ruby ac-helm ac-etags))))
+    (yafolding twittering-mode ruby-end ruby-electric ruby-compilation ruby-additional projectile-codesearch popwin pallet neotree markdown-mode magit-svn magit-find-file id-manager helm-swoop helm-rb helm-projectile helm-package helm-migemo helm-ls-svn helm-ls-hg helm-ls-git helm-gtags helm-go-package helm-gitignore helm-git-grep helm-git-files helm-git helm-gist helm-flycheck helm-ag go-projectile ac-js2 ac-inf-ruby ac-helm ac-etags))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
