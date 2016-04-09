4ede6999f31d83bad681c37f35a73d1059cfbda8
nabeo <watanabe.michikazu@gmail.com>
2016-04-09 14:37:34 +0900

    ignore alpha of default-frame-alist

--- cocoa/emacs-themes.el
+++ cocoa/emacs-themes.el
@@ -1,7 +1,4 @@
-;; Last update is ...
-;; Time-stamp: "Thu May 02 23:22:47 JST 2013"
-
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (load-theme 'manoj-dark t)
 
-(add-to-list 'default-frame-alist '(alpha . (85 50)))
+;; (add-to-list 'default-frame-alist '(alpha . (85 50)))
