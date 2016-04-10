6fb50e90d9b7218fd0fd46cfc79ac1e54b415798
nabeo <watanabe.michikazu@gmail.com>
2016-04-10 13:33:38 +0900

    refactor for helm

--- cocoa/emacs-helm.el
+++ cocoa/emacs-helm.el
@@ -1,22 +1,23 @@
-;; Last update is ....
-;; Time-stamp: "Sun Nov 15 14:52:35 JST 2015"
-
-;; https://github.com/emacs-helm/helm
-;; (my-load-path "~/site-lisp/cocoa/helm")
-;; -> ~/local/emacs/site-lisp/helm
-;; (my-load-path "~/site-lisp/cocoa/helm-ext")
-;; -> ~/local/emacs/site-lisp/helm-ext
 (require 'helm-config)
 (require 'helm-bbdb)
 (require 'helm-elscreen)
 (helm-mode 1)
+(helm-autoresize-mode 1)
 
-;; from emacs-helm.sh
-(define-key global-map [remap list-buffers] 'helm-buffers-list)
-;; (define-key global-map [remap find-file] 'helm-find-files)
-;; (define-key global-map [remap occur] 'helm-occur)
-;; (define-key lisp-interaction-mode-map [remap indent-for-tab-command] 'helm-lisp-completion-at-point-or-indent)
-;; (define-key emacs-lisp-mode-map       [remap indent-for-tab-command] 'helm-lisp-completion-at-point-or-indent)
+;; key bind config
+(global-set-key [remap list-buffers] 'helm-buffers-list)
+(global-set-key [remap find-file] 'helm-find-files)
+(global-set-key [remap occur] 'helm-occur)
+(global-set-key (kbd "M-x") 'helm-M-x)
+;; (define-key lisp-interaction-mode-map
+;;   [remap indent-for-tab-command]
+;;   'helm-lisp-completion-at-point-or-indent)
+;; (define-key emacs-lisp-mode-map
+;;   [remap indent-for-tab-command]
+;;   'helm-lisp-completion-at-point-or-indent)
+
+(define-key helm-map (kbd "C-h") 'delete-backward-char)
+(define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
 
 ;; helm-etags+
 ;; https://github.com/jixiuf/helm-etags-plus
@@ -47,35 +48,20 @@
 ;;             helm-default-info-index-list))
 
 ;; helm-sources
-(setq helm-sources (list helm-c-source-buffers-list
-                         helm-c-source-files-in-current-dir
-                         helm-c-source-file-name-history
-                         helm-c-source-file-cache
-                         helm-c-source-etags-select
-                         ;; helm-c-source-locate
-                         ;; helm-c-source-info-id-utils
-                         ;; helm-c-source-info-pages
-                         helm-source-bbdb
-                         helm-c-source-elscreen))
+;; (setq helm-sources (list
+;;                     helm-source-files-in-current-dir
+;;                     helm-source-file-name-history
+;;                     helm-source-file-cache
+;;                     helm-source-buffers-list
+;;                     helm-source-locate
+;;                     helm-source-bbdb
+;;                     helm-source-elscreen
+;; ))
 
-;; keymap
+;; helm-elscreen
 (eval-after-load "elscreen"
   '(progn
-     (define-key global-map (kbd "C-z SPC") 'helm-elscreen)))
-
-
-;; override kdb
-(eval-after-load "helm-files"
-  '(progn
-     (define-key helm-find-files-map (kbd "C-h C-b") nil)
-     (define-key helm-find-files-map (kbd "C-h C-d") nil)
-     (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)))
-(eval-after-load "helm"
-  '(progn
-     (define-key helm-map (kbd "C-h C-b") nil)
-     (define-key helm-map (kbd "C-h C-h") nil)
-     (define-key helm-map (kbd "C-h h") nil)
-     (define-key helm-map (kbd "C-h") 'delete-backward-char)))
+     (global-set-key (kbd "C-z SPC") 'helm-elscreen)))
 
 ;; helm-swoop
 (cond ((locate-library "helm-swoop")
@@ -88,40 +74,15 @@
        ;; from helm-swoop to helm-multi-swoop-all
        (define-key helm-swoop-map (kbd "M-i") 'helm-multi-swoop-all-from-helm-swoop)))
 
-;; anything-books.el
-;; https://github.com/kiwanami/emacs-anything-books
-;; http://d.hatena.ne.jp/kiwanami/20101117/1290015869
-;; http://d.hatena.ne.jp/kiwanami/20101126/1290765338
-     
-;; (require 'anything-books)
-;; (setq abks:books-dir "~/Documents/pdf") ; PDFファイルを格納しているディレクトリ
-;; (global-set-key (kbd "M-8") 'helm-books-command)
-;; (setq abks:cache-pixel "600")
-;; (setq abks:mkcover-cmd '("qlmanage" "-t" pdf "-s" size "-o" dir))
-;; (setq abks:mkcover-image-ext ".png")
-;; (setq abks:open-command "open") ; 標準のプログラムを使う
-
-;; howm と anything-books.el を連携させる
-;; (eval-after-load "howm"
-;;   '(progn
-;;      (defvar abks:howm-title-format   '("BOOK " title))
-;;      (defvar abks:howm-content-format '(">>>" file))
-;;
-;;      (defun abks:howm-open (file)
-;;        (let* ((data `((title . ,(hbks:file-to-title file)) (file . ,file)))
-;;               (howm-title (apply 'concat (abks:list-template abks:howm-title-format data)))
-;;               (howm-content (apply 'concat (abks:list-template abks:howm-content-format data)))
-;;               (howm-items (howm-folder-grep howm-directory (regexp-quote howm-title))))
-;;          (cond
-;;           ((null howm-items) ; create
-;;            (howm-create-file-with-title howm-title nil nil nil howm-content))
-;;           ((eql 1 (length howm-items)) ; open
-;;            (howm-view-open-item (car howm-items)))
-;;           (t ; list
-;;            (howm-view-summary "Helm Books" howm-items)))))
-;; 
-     ;; register howm action
-;;      (setq anything-books-actions 
-;;            (append anything-books-actions
-;;                    '(("Open a howm item" . (lambda (f) (abks:howm-open f))))))
-;;      ))
+;; helm-ghq
+(cond ((locate-library "helm-ghq")
+       (require 'helm-ghq)
+       (if (file-executable-p "~/Development/golang/bin/ghq")
+           (setq helm-ghq-command-ghq "~/Development/golang/bin/ghq"))
+       (if (file-executable-p "/opt/local/bin/git")
+           (setq helm-ghq-command-git "/opt/local/bin/git"))
+       (if (file-executable-p "/opt/local/bin/hg")
+           (setq helm-ghq-command-hg "/opt/local/bin/hg"))
+       (if (file-executable-p "/opt/local/bin/svn")
+           (setq helm-ghq-command-svn "/opt/local/bin/svn"))
+       ))
