8fd82f5c6047e0975dbb1fb9ff32d298d3ea6dc7
nabeo <watanabe.michikazu@gmail.com>
2016-08-13 17:32:17 +0900

    update golang config

--- emacs-golang.el
+++ emacs-golang.el
@@ -6,11 +6,23 @@
     (setenv "GOPATH" (expand-file-name "~/Development/golang")))
 
 ;; set PATH
-(if (file-exists-p (concat (getenv "GOPATH") "/bin"))
-    (setq exec-path (cons (concat (getenv "GOPATH") "/bin") exec-path)))
+(cond ((file-exists-p (concat (getenv "GOPATH") "/bin"))
+       (setq exec-path (append exec-path '((concat (getenv "GOPATH") "/bin"))))
+       (setenv "PATH" (concat (getenv "PATH") ":" (concat (getenv "GOPATH") "/bin")))
+       ))
 
-(with-eval-after-load 'go-mode
-  (require 'go-autocomplete)
-  (add-hook 'go-mode-hook 'go-eldoc-setup)
-  (define-key go-mode-map (kbd "M-.") 'godef-jump)
-  (define-key go-mode-map (kbd "M-,") 'pop-tag-mark))
+;; set commands
+(setq go-command "/opt/local/bin/go")
+(setq gofmt-command "/opt/local/bin/gofmt")
+(setq godoc-command "/opt/local/bin/go doc")
+(setq godef-command "/Users/nabeo/Development/golang/bin/godef")
+(setq godoc-and-godef-command "/Users/nabeo/Development/golang/bin/godoc")
+
+(cond ((locate-library "go-autocomplete")
+       (require 'go-autocomplete)
+       (require 'auto-complete-config)))
+(cond ((locate-library "go-eldoc")
+       (add-hook 'go-mode-hook 'go-eldoc-setup)))
+
+(add-hook 'go-mode-hook (lambda ()
+                          (local-set-key (kbd "M-.") 'godef-jump)))
