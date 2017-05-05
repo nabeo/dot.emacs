435816d07fe86bd9e933429ed104994eb282f21a
nabeo <watanabe.michikazu@gmail.com>
2017-05-05 18:22:09 +0900

    use expand-file-name

--- emacs-twittering.el
+++ emacs-twittering.el
@@ -1,16 +1,12 @@
-;; last update is ....
-;; Time-stamp: "Mon Dec 23 13:56:45 JST 2013"
-
-;; (my-load-path "~/site-lisp/twitter-mode")
-;; -> ~/local/emacs/site-lisp/twitter-mode
-
 (require 'twittering-mode)
 ;; for debuggin (ignore certficate file)
 ;; (setq twittering-allow-insecure-server-cert t)
 
 (setq twittering-use-master-password t)
-(setq twittering-private-info-file "/Users/nabeo/Dropbox/emacs/twittering-mode/twittering-mode-oauth.gpg")
+(setq twittering-private-info-file
+      (expand-file-name "~/Dropbox/emacs/twittering-mode/twittering-mode-oauth.gpg"))
 
 (auto-compression-mode t)
-(setq twittering-icon-storage-file "/Users/nabeo/.emacs.d/twittering-mode/twittering-mode-icons.gz")
+(setq twittering-icon-storage-file
+      (expand-file-name "~/.emacs.d/twittering-mode/twittering-mode-icons.gz"))
 (setq twittering-use-icon-storage t)
