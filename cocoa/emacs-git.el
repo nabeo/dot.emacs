7493653fe25f83370ce4c9645f7a04b3e1c6cf7f
nabeo <watanabe.michikazu@gmail.com>
2017-05-05 16:39:15 +0900

    add git-gutter+

--- cocoa/emacs-git.el
+++ cocoa/emacs-git.el
@@ -1,2 +1,23 @@
 (autoload 'magit-status "magit" nil t)
 (global-set-key (kbd "C-x g") 'magit-status)
+
+(cond ((locate-library "git-gutter+")
+       ;; (global-git-gutter+-mode)
+       (add-hook 'prog-mode-hook 'git-gutter+-mode)
+       (add-hook 'yaml-mode-hook 'git-gutter+-mode)
+       (eval-after-load 'git-gutter+
+         '(progn
+            ;; Jump between hunks
+            (define-key git-gutter+-mode-map (kbd "C-x n") 'git-gutter+-next-hunk)
+            (define-key git-gutter+-mode-map (kbd "C-x p") 'git-gutter+-previous-hunk)
+            ;; Act on hunks
+            (define-key git-gutter+-mode-map (kbd "C-x v =") 'git-gutter+-show-hunk)
+            (define-key git-gutter+-mode-map (kbd "C-x r") 'git-gutter+-revert-hunks)
+            ;; Stage hunk at point.
+            ;; If region is active, stage all hunk lines within the region.
+            (define-key git-gutter+-mode-map (kbd "C-x t") 'git-gutter+-stage-hunks)
+            (define-key git-gutter+-mode-map (kbd "C-x c") 'git-gutter+-commit)
+            (define-key git-gutter+-mode-map (kbd "C-x C") 'git-gutter+-stage-and-commit)
+            (define-key git-gutter+-mode-map (kbd "C-x C-y") 'git-gutter+-stage-and-commit-whole-buffer)
+            (define-key git-gutter+-mode-map (kbd "C-x U") 'git-gutter+-unstage-whole-buffer)
+            ))))
