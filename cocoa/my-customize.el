a262f682a5923651c03f243f5e2bd1910d18e4f8
nabeo <watanabe.michikazu@gmail.com>
2017-05-05 18:16:50 +0900

    update packages

--- cocoa/my-customize.el
+++ cocoa/my-customize.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (use-package yaml-mode yafolding wanderlust vdiff twittering-mode ruby-end ruby-electric ruby-compilation ruby-additional popwin pallet markdown-mode magit-svn magit-find-file ipcalc id-manager howm helm-swoop helm-rb helm-projectile helm-package helm-migemo helm-ls-svn helm-ls-hg helm-ls-git helm-gtags helm-go-package helm-gitignore helm-git-grep helm-git-files helm-git helm-ghq helm-flycheck helm-ag go-projectile go-autocomplete git-gutter+ git gist fuzzy auctex ac-js2 ac-inf-ruby ac-helm ac-etags))))
+    (yaml-mode yafolding wanderlust vdiff use-package twittering-mode ruby-end ruby-electric ruby-compilation ruby-additional popwin pallet markdown-mode magit-svn magit-find-file ipcalc id-manager howm helm-swoop helm-rb helm-projectile helm-package helm-migemo helm-ls-svn helm-ls-hg helm-ls-git helm-gtags helm-go-package helm-gitignore helm-git-grep helm-git-files helm-git helm-ghq helm-flycheck helm-ag go-projectile go-autocomplete git-gutter+ git gist fuzzy auctex ac-js2 ac-inf-ruby ac-helm ac-etags))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
