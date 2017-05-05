40c28e54527f8db591cec4cc7fe29ed66a5883dd
nabeo <watanabe.michikazu@gmail.com>
2017-05-05 16:19:31 +0900

    update for emacs-25

--- init.el
+++ init.el
@@ -4,9 +4,6 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
-;; last update is ....
-;; Time-stamp: "Wed Aug 12 23:46:24 JST 2015"
-
 (load "~/.emacs.d/emacs-my.el")
 
 (cond (emacs22-p ;; Carbon Emacs用
@@ -14,7 +11,7 @@
       ;; ((featurep 'aquamacs) ;; Aquamacs用 -> 使えない
       ;;  (load-safe "~/.emacs.d/emacs-aquamacs.el")
       ;;  (load-safe "~/.emacs.d/emacs-cocoa.el")
-      ((or emacs23-p emacs24-p emacs-bzr-p) ;; Cocoa Emacs用
+      ((or emacs23-p emacs24-p emacs25-p emacs-bzr-p) ;; Cocoa Emacs用
        (load-safe "~/.emacs.d/emacs-cocoa.el")))
 
 (global-font-lock-mode t)
