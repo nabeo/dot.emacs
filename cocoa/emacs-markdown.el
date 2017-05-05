26370f5755775f53b166dbefd5ef7a40f66148ae
nabeo <watanabe.michikazu@gmail.com>
2017-05-05 17:42:20 +0900

    markdown-mode: maigrate use-package

--- cocoa/emacs-markdown.el
+++ cocoa/emacs-markdown.el
@@ -1,16 +1,7 @@
-(autoload 'markdown-mode "markdown-mode.el"
-  "Major mode for editing Markdown files" t)
-
-(add-to-list 'auto-mode-alist '("\\.mdwn" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.mdt" . markdown-mode))
-
-;; http://d.hatena.ne.jp/kitokitoki/20120206/p1
-(add-hook 'markdown-mode-hook
-  (lambda()
-    (define-key markdown-mode-map (kbd "C-i") 'markdown-cycle)
-    (hide-sublevels 2)))
-(add-hook 'gfm-mode-hook
-  (lambda()
-    (define-key markdown-mode-map (kbd "C-i") 'markdown-cycle)
-    (hide-sublevels 2)))
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("\\.mdwn" . markdown-mode)
+         ("\\.md" . markdown-mode)
+         ("\\.mdt" . markdown-mode))
+  :init ())
