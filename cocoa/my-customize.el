489372e42c757a0d3a3166bfb2253579d4e4110e
nabeo <watanabe.michikazu@gmail.com>
2017-11-11 18:08:58 +0900

    package.el: install package at 729a2

--- cocoa/my-customize.el
+++ cocoa/my-customize.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (ruby-style helm-etags+ yafolding wanderlust w3m use-package twittering-mode ruby-electric ruby-block popwin migemo markdown-mode magit init-loader id-manager howm helm-swoop helm-projectile helm-ghq helm-elscreen helm-c-yasnippet go-projectile go-autocomplete git-gutter+ fuzzy direx))))
+    (ac-etags ac-helm ac-inf-ruby ac-js2 auctex cargo gist git go-snippets ipcalc -find-file pallet ruby-additional ruby-compilation ruby-end rust-playground toml-mode vdiff yaml-mode yafolding wanderlust w3m use-package twittering-mode ruby-electric ruby-block popwin markdown-mode magit-svn magit-find-file init-loader id-manager howm helm-swoop helm-rb helm-projectile helm-package helm-migemo helm-ls-svn helm-ls-hg helm-ls-git helm-gtags helm-go-package helm-gitignore helm-git-grep helm-git-files helm-git helm-ghq helm-flycheck helm-etags-plus helm-elscreen helm-c-yasnippet helm-ag go-projectile go-autocomplete git-gutter+ fuzzy direx))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
