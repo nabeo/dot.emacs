32ff13693060f0f03231d627fb23fec524c8a640
nabeo <watanabe.michikazu@gmail.com>
2017-11-11 12:31:57 +0900

    inits/20-migemo.el: copy from emacs-migemo.el

---
+++ inits/20-migemo.el
@@ -0,0 +1,16 @@
+(require 'migemo)
+;; (load "~/site-lisp/migemo.el")
+;; cmigemoを使うための設定
+(setq migemo-command "/opt/local/bin/cmigemo")
+(setq migemo-options '("-q" "--emacs"))
+(setq migemo-dictionary "/opt/local/share/migemo/utf-8/migemo-dict")
+(setq migemo-user-ditionary nil)
+(setq migemo-regex-dictionary nil)
+(setq migemo-coding-system 'utf-8-unix)
+
+;; Emacs 側のキャッシュをクリアする
+(setq migemo-use-pattern-alist t)
+(setq migemo-use-frequent-pattern-alist t)
+
+;; migemoの初期化
+(migemo-init)
