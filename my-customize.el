af45e627be01c848392769e77d705cde1bd864dc
nabeo <watanabe.michikazu@gmail.com>
2017-11-12 23:38:10 +0900

    update package-selected-packages

--- my-customize.el
+++ my-customize.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (ac-etags ac-helm ac-inf-ruby ac-js2 auctex cargo gist git go-snippets ipcalc -find-file pallet ruby-additional ruby-compilation ruby-end rust-playground toml-mode vdiff yaml-mode yafolding wanderlust w3m use-package twittering-mode ruby-electric ruby-block popwin markdown-mode magit-svn magit-find-file init-loader id-manager howm helm-swoop helm-rb helm-projectile helm-package helm-migemo helm-ls-svn helm-ls-hg helm-ls-git helm-gtags helm-go-package helm-gitignore helm-git-grep helm-git-files helm-git helm-ghq helm-flycheck helm-etags-plus helm-elscreen helm-c-yasnippet helm-ag go-projectile go-autocomplete git-gutter+ fuzzy direx))))
+    (cp5022x yaml-mode yafolding wanderlust w3m vdiff use-package twittering-mode toml-mode rust-playground ruby-end ruby-electric ruby-compilation ruby-block ruby-additional popwin pallet markdown-mode magit-svn magit-find-file ipcalc init-loader id-manager howm helm-swoop helm-rb helm-projectile helm-package helm-migemo helm-ls-svn helm-ls-hg helm-ls-git helm-gtags helm-go-package helm-gitignore helm-git-grep helm-git-files helm-git helm-ghq helm-flycheck helm-etags-plus helm-elscreen helm-c-yasnippet helm-ag go-snippets go-projectile go-autocomplete git-gutter+ git gist fuzzy direx cargo auctex ac-js2 ac-inf-ruby ac-helm ac-etags))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
