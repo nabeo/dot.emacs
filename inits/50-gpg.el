99b8e4ef65961726d6ea108a9d2522214fc4fbdd
nabeo <watanabe.michikazu@gmail.com>
2018-02-08 00:28:18 +0900

    id-manager move to 50-gpg.el

--- inits/50-gpg.el
+++ inits/50-gpg.el
@@ -13,4 +13,19 @@
   
 (use-package auth-source
   :init
-  (setq auth-sources '("~/.emacs.d/secret/authinfo.gpg")))
+  (setq auth-sources '("~/.emacs.d/secret/authinfo.gpg"))
+  )
+
+(use-package id-manager
+  :ensure t
+  :init
+  ;; EasyPGで使うための設定
+  ;; minibufferでパスワードを入力する場合
+  (setenv "GPG_AGENT_INFO" nil)
+  :config
+  ;; パスワードを保存する
+  ;; (setq epa-file-cache-passphrase-for-symmetric-encryption t)
+
+  ;; パスワードの保存先(gpgで暗号化されている)
+  (setq idm-database-file "~/.emacs.d/secret/idm-db.gpg")
+  )
