ce9e1b912d055e377b1e870a99b57f5e21da0642
nabeo <watanabe.michikazu@gmail.com>
2018-02-08 00:51:48 +0900

    update gpg

--- inits/50-gpg.el
+++ inits/50-gpg.el
@@ -1,8 +1,6 @@
 (use-package epa
   :config
   (epa-file-enable)
-  ;; MacPortsでgnupg2をインストールしている場合
-  ;; (setq epg-gpg-program "/opt/local/bin/gpg2")
   )
 
 (use-package pinentry
@@ -13,19 +11,14 @@
   
 (use-package auth-source
   :init
-  (setq auth-sources '("~/.emacs.d/secret/authinfo.gpg"))
+  (add-to-list 'auth-sources
+               (expand-file-name
+                (concat user-emacs-directory "secret/authinfo.gpg")))
   )
 
 (use-package id-manager
   :ensure t
   :init
-  ;; EasyPGで使うための設定
-  ;; minibufferでパスワードを入力する場合
-  (setenv "GPG_AGENT_INFO" nil)
-  :config
-  ;; パスワードを保存する
-  ;; (setq epa-file-cache-passphrase-for-symmetric-encryption t)
-
   ;; パスワードの保存先(gpgで暗号化されている)
   (setq idm-database-file "~/.emacs.d/secret/idm-db.gpg")
   )
