dd8eb7900449e4672c062cb13e46d418f689994f
nabeo <watanabe.michikazu@gmail.com>
2018-02-10 14:55:24 +0900

    use migemo, if have executable cmigemo

--- inits/20-migemo.el
+++ inits/20-migemo.el
@@ -1,10 +1,11 @@
 (use-package migemo
   :ensure t
+  :if (file-executable-p "/usr/local/bin/cmigemo")
   :config
   ;; cmigemoを使うための設定
-  (setq migemo-command "/opt/local/bin/cmigemo")
+  (setq migemo-command "/usr/local/bin/cmigemo")
   (setq migemo-options '("-q" "--emacs"))
-  (setq migemo-dictionary "/opt/local/share/migemo/utf-8/migemo-dict")
+  (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
   (setq migemo-user-ditionary nil)
   (setq migemo-regex-dictionary nil)
   (setq migemo-coding-system 'utf-8-unix)
