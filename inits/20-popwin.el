06c8272cf70dc70bb58ca4886720c186c92cd15f
nabeo <watanabe.michikazu@gmail.com>
2018-02-10 11:44:12 +0900

    popwin: change load order

---
+++ inits/20-popwin.el
@@ -0,0 +1,23 @@
+(use-package popwin
+  :ensure t
+  :config
+  (global-set-key (kbd "C-x p") 'popwin:keymap)
+  (popwin-mode 1)
+
+  (setq display-buffer-function 'popwin:display-buffer)
+
+  (setq anything-samewindow nil)
+  (setq helm-samewindow nil)
+
+  (push '("*anything*" :height 20) popwin:special-display-config)
+  (push '("*Apropos*") popwin:special-display-config)
+  (push '("*sdic*") popwin:special-display-config)
+  (push '("*Faces*") popwin:special-display-config)
+  (push '("*Colors*") popwin:special-display-config)
+
+  ;; id-manager
+  (push '("ID-Password List") popwin:special-display-config)
+
+  ;; bbdb
+  (push '("*BBDB*") popwin:special-display-config)
+  )
