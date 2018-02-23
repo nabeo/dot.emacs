994afd9315150b0480fd1e44074afa8245c55cb4
nabeo <watanabe.michikazu@gmail.com>
2018-02-23 16:39:13 +0900

    update faces

---
+++ inits/10-theme.el
@@ -0,0 +1,23 @@
+(use-package all-the-icons
+  :ensure t
+  :config
+  (add-to-list 'custom-theme-load-path (concat user-emacs-directory "themes"))
+  )
+
+(use-package doom-themes
+  :ensure t
+  :config
+  (load-theme 'doom-one t) ;; or doom-dark, etc.
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; for neotree
+  (doom-themes-neotree-config)
+  (setq doom-neotree-enable-file-icons t)
+  (setq doom-neotree-enable-folder-icons t)
+  (setq doom-neotree-enable-chvron-icons t)
+  (setq doom-neotree-file-icons t)
+  (setq doom-neotree-project-size 1)
+  (setq doom-neotree-folder-size 0.8)
+  (setq doom-neotree-chevron-size 0.8)
+  (setq doom-neotree-line-spacing 1)
+  )
--- inits/50-themes.el
+++
@@ -1,4 +0,0 @@
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-(load-theme 'manoj-dark t)
-
-;; (add-to-list 'default-frame-alist '(alpha . (85 50)))
