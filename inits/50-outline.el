caea2b7e7798eb2ab5a4009631048fe86cc5b014
nabeo <watanabe.michikazu@gmail.com>
2018-02-23 16:39:40 +0900

    add locate-library

--- inits/50-outline.el
+++ inits/50-outline.el
@@ -1,6 +1,3 @@
-;; last update is ....
-;; Time-stamp: "Sat Jan 12 22:39:10 JST 2013"
-
 ;; misc setting
 (make-variable-buffer-local 'outline-level)
 (setq-default outline-level 'outline-level)
@@ -15,10 +12,11 @@
 ;; http://d.hatena.ne.jp/kitokitoki/20100517/p2
 ;; emacswikiから
 ;; windata.el, tree-mode.el, imenu-tree.el をダウンロードする
-(add-hook 'outline-mode-hook
-          (lambda ()
-            (require 'imenu-tree))
-          (global-set-key (kbd "M-h") 'imenu-tree))
+(cond ((locate-library "imenu-tree")
+       (add-hook 'outline-mode-hook
+                 (lambda ()
+                   (require 'imenu-tree))
+                 (global-set-key (kbd "M-h") 'imenu-tree))))
 
 ;; just define a hook for the minor mode that just defines an additional prefix key.
 ;; http://emacswiki.org/emacs/OutlineMinorMode
@@ -28,8 +26,9 @@
 
 ;; http://www.emacswiki.org/emacs/download/outline-magic.el
 ;; http://www.emacswiki.org/emacs/OutlineMagic
-(eval-after-load 'outline
-  '(progn
-    (require 'outline-magic)
-    (define-key outline-minor-mode-map (kbd "<backtab>") 'outline-cycle)))
+(cond ((locate-library "outline-magic")
+       (eval-after-load 'outline
+         '(progn
+            (require 'outline-magic)
+            (define-key outline-minor-mode-map (kbd "<backtab>") 'outline-cycle)))))
 
