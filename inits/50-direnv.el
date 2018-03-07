fef378d99d5af693092890f969b3260d07f8c904
Michikazu Watanabe <nabeop@hatena.ne.jp>
2018-03-07 18:10:49 +0900

    inits/50-direnv.el: search path env

--- inits/50-direnv.el
+++ inits/50-direnv.el
@@ -1,6 +1,7 @@
 (use-package direnv
   :ensure t
-  :if (file-executable-p (concat (getenv "GOPATH") "/bin/direnv"))
+  :if (or (executable-find "direnv")
+          (file-executable-p (concat (getenv "GOPATH") "/bin/direnv")))
   :config
   (direnv-mode)
   (setq direnv-always-show-summary t))
