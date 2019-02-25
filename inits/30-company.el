2bb09d1966c3eb0174b64138094163bebaa46091
Michikazu Watanabe <nabeop@hatena.ne.jp>
2019-02-26 00:50:39 +0900

    split lsp-mode

--- inits/30-company.el
+++ inits/30-company.el
@@ -21,33 +21,15 @@
   (global-company-mode)
   :config
   (setq company-idle-delay 0)
-  ;; (setq company-echo-delay 0)
+  (setq company-echo-delay 0)
 
-  ;; (use-package company-quickhelp
-  ;;   :ensure t
-  ;;   :init
-  ;;   (company-quickhelp-mode)
-  ;;   :config
-  ;;   (setq company-quickhelp-delay nil))
+  (use-package company-quickhelp
+    :ensure t
+    :init
+    (company-quickhelp-mode)
+    :config
+    (setq company-quickhelp-delay nil))
   )
-
-(use-package lsp-mode
-  :custom ((lsp-inhibit-message t)
-         (lsp-message-project-root-warning t)
-         (create-lockfiles nil))
-  :hook   (prog-major-mode . lsp-prog-major-mode-enable))
-
-(use-package lsp-ui
-  :after lsp-mode
-  :custom (scroll-margin 0)
-  :hook   (lsp-mode . lsp-ui-mode))
-
-(use-package company-lsp
-  :after (lsp-mode company)
-  :defines company-backends
-  :config
-  (push 'company-lsp company-backends)
-)
 
 (provide '30-company)
 ;;; 30-company ends here
---
+++ inits/30-lsp.el
@@ -0,0 +1,23 @@
+;;; 30-lsp --- lsp-mode
+;;; Commentary:
+
+;;; Code:
+(use-package lsp-mode
+  :custom ((lsp-inhibit-message t)
+         (lsp-message-project-root-warning t)
+         (create-lockfiles nil))
+  :hook (prog-major-mode . lsp-prog-major-mode-enable))
+
+(use-package lsp-ui
+  :after lsp-mode
+  :custom (scroll-margin 0)
+  :hook   (lsp-mode . lsp-ui-mode))
+
+(use-package company-lsp
+  :after (lsp-mode company)
+  :defines company-backends
+  :config
+  (push 'company-lsp company-backends))
+
+(provide '30-lsp)
+;;; 30-lsp ends here
