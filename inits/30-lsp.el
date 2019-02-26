16bf8330e661b6d86f279f1caabebc0e55e1d37e
Michikazu Watanabe <nabeop@hatena.ne.jp>
2019-02-26 11:22:51 +0900

    update 30-lsp

--- inits/30-lsp.el
+++ inits/30-lsp.el
@@ -3,26 +3,33 @@
 
 ;;; Code:
 (use-package lsp-mode
-  :custom ((lsp-inhibit-message t)
-         (lsp-message-project-root-warning t)
-            (create-lockfiles nil))
+  :custom
+  ((lsp-inhibit-message t)
+    (lsp-message-project-root-warning t)
+    (create-lockfiles nil)
+    (lsp-auto-guess-root t))
   :init
   (require 'lsp-clients)
-  :hook ((prog-major-mode . lsp-prog-major-mode-enable)
-          (go-mode . lsp)
-          (python-mode . lsp)
-          ))
+  :hook
+  ((prog-major-mode . lsp-prog-major-mode-enable)
+    ;; go get -u github.com/saibing/bingo
+    (go-mode . lsp)
+    ;; pip install 'python-language-server[all]'
+    (python-mode . lsp))
+  )
 
 (use-package lsp-ui
   :after lsp-mode
   :custom (scroll-margin 0)
-  :hook   (lsp-mode . lsp-ui-mode))
+  :hook   (lsp-mode . lsp-ui-mode)
+  )
 
 (use-package company-lsp
   :after (lsp-mode company)
   :defines company-backends
   :config
-  (push 'company-lsp company-backends))
+  (push 'company-lsp company-backends)
+  )
 
 (provide '30-lsp)
 ;;; 30-lsp ends here
