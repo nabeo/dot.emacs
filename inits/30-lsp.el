21aee072a44d3376c120f585164e3fabb1007507
Michikazu Watanabe <nabeop@hatena.ne.jp>
2019-02-28 19:53:26 +0900

    update 30-lsp.el

--- inits/30-lsp.el
+++ inits/30-lsp.el
@@ -18,13 +18,18 @@
     (python-mode . lsp)
     ;; npm i -g typescript-language-server; npm i -g typescript
     (typescript-mode . lsp)
-    (javascript-mode . lsp))
+    (javascript-mode . lsp)
+    ;; gem install solargraph
+    (ruby-mode . lsp))
   )
 
 (use-package lsp-ui
   :after lsp-mode
   :custom (scroll-margin 0)
   :hook   (lsp-mode . lsp-ui-mode)
+  :config
+  (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+  (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
   )
 
 (use-package company-lsp
