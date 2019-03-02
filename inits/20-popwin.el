89e16c6dc27907d9626e6c96047979788f2b574b
Michikazu Watanabe <nabeop@hatena.ne.jp>
2019-03-02 17:56:40 +0900

    update 20-popwin

--- inits/20-popwin.el
+++ inits/20-popwin.el
@@ -1,13 +1,13 @@
+;;;  20-popwin --- popwin
+;;; Commentary:
+
+;;; Code:
 (use-package popwin
   :ensure t
   :config
   (global-set-key (kbd "C-x p") 'popwin:keymap)
   (popwin-mode 1)
 
-  (setq anything-samewindow nil)
-  (setq helm-samewindow nil)
-
-  (push '("*anything*" :height 20) popwin:special-display-config)
   (push '("*Apropos*") popwin:special-display-config)
   (push '("*sdic*") popwin:special-display-config)
   (push '("*Faces*") popwin:special-display-config)
@@ -16,3 +16,6 @@
   ;; bbdb
   (push '("*BBDB*") popwin:special-display-config)
   )
+
+(provide '20-popwin)
+;;; 20-popwin ends here
