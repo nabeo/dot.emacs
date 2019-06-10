92d56fc5bf261f87d66e25ca5b14d206961e3e7b
Michikazu Watanabe <nabeop@hatena.ne.jp>
2019-06-10 14:47:02 +0900

    add groovy-mode

---
+++ inits/50-groovy.el
@@ -0,0 +1,21 @@
+;;; 50-groovy --- groovy
+;;; Commentary:
+
+;;; Code:
+(use-package groovy-mode
+  :ensure t
+  :mode
+  ( ("\\.groovy\\'" . groovy-mode)
+    ("\\.gradle\\'" . groovy-mode)
+    ("Jenkinsfile\\'" . groovy-mode))
+  :interpreter
+  (("groovy" . groovy-mode))
+  :config
+  (add-hook 'groovy-mode-hook
+    '(lambda ()
+       (require 'groovy-electric)
+       (groovy-electric-mode)))
+  )
+
+(provide '50-groovy)
+;;; 50-groovy ends here
