4e0f93474fcf0e8e3f486a36ebd790de50c090f7
Michikazu Watanabe <nabeop@hatena.ne.jp>
2019-12-19 14:46:25 +0900

    use highlight-indent-guides

---
+++ inits/50-highlight-indent-guides.el
@@ -0,0 +1,16 @@
+;;; 50-highlight-indent-guides --- highlight-indent-guides.el
+;;; Commentary:
+
+;;; Code:
+(use-package highlight-indent-guides
+  :diminish
+  :hook
+  ((prog-mode yaml-mode) . highlight-indent-guides-mode)
+  :custom
+  (highlight-indent-guides-auto-enabled t)
+  (highlight-indent-guides-responsive t)
+  (highlight-indent-guides-method 'character)
+  )
+
+(provide '50-highlight-indent-guides)
+;;; 50-highlight-indent-guides ends here
