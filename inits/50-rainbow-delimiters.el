4c63835b6b984f1e6ab6d762d82cf884f7aba630
Michikazu Watanabe <nabeop@hatena.ne.jp>
2019-12-19 14:46:36 +0900

    use rainbow-delimiters

---
+++ inits/50-rainbow-delimiters.el
@@ -0,0 +1,10 @@
+;;; 50-rainbow-delimiters --- rainbow-delimiters.el
+;;; Commentary:
+
+;;; Code:
+(use-package rainbow-delimiters
+  :hook
+  (prog-mode . rainbow-delimiters-mode))
+
+(provide '50-rainbow-delimiters)
+;;; 50-rainbow-delimiters ends here
