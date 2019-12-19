0d372f05b2822089a6f60b572b9bc6c90018681f
Michikazu Watanabe <nabeop@hatena.ne.jp>
2019-12-19 16:30:12 +0900

    inits/10-theme

--- inits/10-theme.el
+++ inits/10-theme.el
@@ -16,15 +16,33 @@
   (doom-themes-visual-bell-config)
   ;; for neotree
   (doom-themes-neotree-config)
-  (setq doom-neotree-enable-file-icons t)
-  (setq doom-neotree-enable-folder-icons t)
-  (setq doom-neotree-enable-chvron-icons t)
-  (setq doom-neotree-file-icons t)
-  (setq doom-neotree-project-size 1)
-  (setq doom-neotree-folder-size 0.8)
-  (setq doom-neotree-chevron-size 0.8)
-  (setq doom-neotree-line-spacing 1)
+  ;; (setq doom-neotree-enable-file-icons t)
+  ;; (setq doom-neotree-enable-folder-icons t)
+  ;; (setq doom-neotree-enable-chvron-icons t)
+  ;; (setq doom-neotree-file-icons t)
+  ;; (setq doom-neotree-project-size 1)
+  ;; (setq doom-neotree-folder-size 0.8)
+  ;; (setq doom-neotree-chevron-size 0.8)
+  ;; (setq doom-neotree-line-spacing 1)
   )
+
+(use-package doom-modeline
+  :commands (doom-modeline-def-modeline)
+  :hook
+  (after-init . doom-modeline-mode)
+  :custom
+  (doom-modeline-buffer-file-name-style 'truncate-with-project)
+  (doom-modeline-icon t)
+  (doom-modeline-major-mode-icon nil)
+  (doom-modeline-minor-modes nil)
+  :config
+  (line-number-mode 0)
+  (column-number-mode 0)
+  (doom-modeline-def-modeline
+    'main
+    '(bar window-number matches buffer-info remote-host buffer-position parrot selection-info)
+    '(misc-info persp-name debug minor-modes input-method major-mode process vcs checker))
+)
 
 (provide '10-theme)
 ;;; 10-theme ends here
