7f468a4bb7412e38650c97a90d2027c3b373e515
Michikazu Watanabe <nabeop@hatena.ne.jp>
2020-03-09 10:57:28 +0900

    use which-key

---
+++ inits/50-which-key.el
@@ -0,0 +1,11 @@
+;;; 50-which-key --- which-key
+;;; Commentary:
+
+;;; Code:
+(use-package which-key
+  :ensure t
+  :diminish which-key-mode
+  :hook (after-init . which-key-mode))
+
+(provide '50-which-key)
+;;; 50-which-key ends here
