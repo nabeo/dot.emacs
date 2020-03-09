86c660bab0c36eb0ea3371dd04c27c38eb08065e
Michikazu Watanabe <nabeop@hatena.ne.jp>
2020-03-09 10:57:57 +0900

    update

--- my-customize.el
+++ my-customize.el
@@ -3,9 +3,54 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(company-box-doc-enable nil)
+ '(company-box-icons-alist (quote company-box-icons-all-the-icons))
+ '(create-lockfiles nil)
+  '(custom-safe-themes
+     (quote
+       ("229c5cf9c9bd4012be621d271320036c69a14758f70e60385e87880b46d60780" "1ed5c8b7478d505a358f578c00b58b430dde379b856fbcb60ed8d345fc95594e" "f7b230ac0a42fc7e93cd0a5976979bd448a857cd82a097048de24e985ca7e4b2" "e47c0abe03e0484ddadf2ae57d32b0f29f0b2ddfe7ec810bd6d558765d9a6a6c" "92d8a13d08e16c4d2c027990f4d69f0ce0833c844dcaad3c8226ae278181d5f3" "4b0b568d63b1c6f6dddb080b476cfba43a8bbc34187c3583165e8fb5bbfde3dc" default)))
+ '(doom-modeline-buffer-file-name-style (quote truncate-with-project))
+ '(doom-modeline-icon t)
+ '(doom-modeline-major-mode-icon nil)
+ '(doom-modeline-minor-modes nil)
+ '(highlight-indent-guides-auto-enabled t)
+ '(highlight-indent-guides-method (quote character))
+ '(highlight-indent-guides-responsive t)
+ '(lsp-auto-guess-root t)
+ '(lsp-clients-clangd-executable "/usr/local/opt/llvm/bin/clangd")
+ '(lsp-clients-javascript-typescript-server "/usr/local/bin/javascript-typescript-stdio" t)
+ '(lsp-clients-typescript-server "/usr/local/bin/typescript-language-server" t)
+ '(lsp-completion-at-point nil t)
+ '(lsp-inhibit-message t t)
+ '(lsp-message-project-root-warning t t)
+ '(lsp-prefer-flymake (quote flymake))
+ '(lsp-pyls-server-command "/Users/nabeop/.pyenv/shims/pyls")
+ '(lsp-ui-doc-alignment (quote window) t)
+ '(lsp-ui-doc-enable t t)
+ '(lsp-ui-doc-header t t)
+ '(lsp-ui-doc-include-signature t t)
+ '(lsp-ui-doc-max-height 30 t)
+ '(lsp-ui-doc-max-width 150 t)
+ '(lsp-ui-doc-position (quote bottom) t)
+ '(lsp-ui-doc-use-childframe t t)
+ '(lsp-ui-doc-use-webkit nil t)
+ '(lsp-ui-flycheck-enable nil t)
+ '(lsp-ui-imenu-enable nil t)
+ '(lsp-ui-imenu-kind-position (quote top) t)
+ '(lsp-ui-peek-enable t t)
+ '(lsp-ui-peek-fontify (quote on-demand) t)
+ '(lsp-ui-peek-list-width 50 t)
+ '(lsp-ui-peek-peek-height 20 t)
+ '(lsp-ui-sideline-enable nil t)
+ '(lsp-ui-sideline-ignore-duplicate t t)
+ '(lsp-ui-sideline-show-code-actions nil t)
+ '(lsp-ui-sideline-show-diagnostics nil t)
+ '(lsp-ui-sideline-show-hover t t)
+ '(lsp-ui-sideline-show-symbol t t)
   '(package-selected-packages
      (quote
-       (doom-modeline company-box rainbow-delimiters highlight-indent-guides rbenv helm-xref bison-mode groovy-mode git-link poly-erb poly-rst poly-ruby poly-ansible poly-markdown typescript-mode company-lsp lsp-ui company-quickhelp company go-dlv helm-mkr doom-themes all-the-icons pinentry flycheck nginx-mode auth-source-pass dockerfile-mode editorconfig json-mode yasnippet semi rust-mode go-eldoc go-mode elscreen migemo ac-racer direnv powerline neotree yasnippet-snippets yaml-mode yafolding web-mode vdiff use-package twittering-mode toml-mode rust-playground ruby-end ruby-electric ruby-compilation ruby-block pyenv-mode projectile-git-autofetch projectile-direnv popwin pass pallet outline-magic markdown-mode magit-svn magit-find-file ipcalc init-loader id-manager ibuffer-projectile howm helm-swoop helm-rb helm-projectile helm-pass helm-package helm-migemo helm-make helm-ls-svn helm-ls-hg helm-ls-git helm-gtags helm-go-package helm-gitignore helm-git-grep helm-git-files helm-git helm-ghq helm-flycheck helm-etags-plus helm-elscreen helm-c-yasnippet helm-ag go-snippets go-projectile go-autocomplete git-gutter+ git gist fuzzy direx diminish cp5022x cargo auctex ac-js2 ac-inf-ruby ac-helm ac-etags))))
+       (which-key helm-lsp lsp-treemacs dap-mode doom-modeline company-box rainbow-delimiters highlight-indent-guides rbenv helm-xref bison-mode groovy-mode git-link poly-erb poly-rst poly-ruby poly-ansible poly-markdown typescript-mode company-lsp lsp-ui company-quickhelp company doom-themes all-the-icons pinentry flycheck nginx-mode auth-source-pass dockerfile-mode editorconfig json-mode yasnippet semi rust-mode go-eldoc go-mode elscreen migemo ac-racer powerline neotree yasnippet-snippets yaml-mode yafolding web-mode vdiff use-package twittering-mode toml-mode rust-playground ruby-end ruby-electric ruby-compilation ruby-block pyenv-mode projectile-direnv popwin pass pallet outline-magic markdown-mode magit-find-file ipcalc init-loader id-manager ibuffer-projectile howm helm-swoop helm-rb helm-projectile helm-pass helm-package helm-migemo helm-make helm-ls-svn helm-ls-hg helm-ls-git helm-gtags helm-go-package helm-gitignore helm-git-grep helm-git-files helm-git helm-ghq helm-flycheck helm-etags-plus helm-elscreen helm-c-yasnippet helm-ag go-snippets go-projectile go-autocomplete git-gutter+ git gist fuzzy direx diminish cp5022x cargo auctex ac-js2 ac-inf-ruby ac-helm ac-etags)))
+ '(scroll-margin 0))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
