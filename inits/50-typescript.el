b241e741427faa3ee0c1de9002cac9d49b6ba8e3
Michikazu Watanabe <nabeop@hatena.ne.jp>
2020-04-02 12:15:23 +0900

    use nodenv

--- inits/50-typescript.el
+++ inits/50-typescript.el
@@ -7,5 +7,15 @@
   :mode ("\\.ts\\'" . typescript-mode)
   )
 
+;; brew install nodenv
+(use-package nodenv
+  :ensure t
+  :hook
+  (js-mode . nodenv-mode)
+  (typescript-mode . nodenv-mode)
+  :init
+  (add-to-list 'exec-path (expand-file-name "~/.nodenv/shims"))
+  )
+
 (provide '50-typescript)
 ;;; 50-typescript ends here
