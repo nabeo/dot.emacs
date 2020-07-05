a963cc4cc95e8d41e8db5b49a72fc3fee7a8363e
nabeo <watanabe.michikazu@gmail.com>
2020-07-05 18:04:50 +0900

    add packages for docker

---
+++ inits/50-docker.el
@@ -0,0 +1,20 @@
+;;; 50-docker --- docker
+;;; Commentary:
+
+;;; Code:
+(use-package docker
+  :ensure t
+  :bind
+  :init
+  :config
+  )
+
+(use-package dockerfile-mode
+  :ensure t
+  :mode
+  ("Dockerfile\\'" . dcokefile-mode)
+  :init
+  :config
+)
+(provide '50-docker)
+;;; 50-docker ends here
