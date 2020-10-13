4645cfa8433d0a878c893228122d850160d7ad5b
Michikazu Watanabe <nabeop@hatena.ne.jp>
2020-10-13 16:35:03 +0900

    CloudFormation 向けの設定

--- inits/50-json.el
+++ inits/50-json.el
@@ -1,5 +1,20 @@
+;;; 50-json --- json-mode
+;;; Commentary:
+
+;;; Code:
 (use-package json-mode
   :ensure t
   :init
   :config
+  ;; https://www.emacswiki.org/emacs/CfnLint
+  (define-derived-mode cfn-json-mode json-mode
+    "CFN-JSON"
+    "Simple mode to edit CloudFormation template in JSON format."
+    (setq js-indent-level 2))
+  (add-to-list 'magic-mode-alist
+    '("\\({\n *\\)? *[\"']AWSTemplateFormatVersion" . cfn-json-mode))
+  (add-hook 'cfn-json-mode-hook 'flycheck-mode)
   )
+
+(provide '50-json)
+;;; 50-json ends here
--- inits/50-yaml.el
+++ inits/50-yaml.el
@@ -1,5 +1,19 @@
+;;; 50-yaml --- yaml-mode
+;;; Commentary:
+
+;;; Code:
 (use-package yaml-mode
   :ensure t
   :init
   :config
+  ;; https://www.emacswiki.org/emacs/CfnLint
+  (define-derived-mode cfn-yaml-mode yaml-mode
+    "CFN-YAML"
+    "Simple mode to edit CloudFormation template in YAML format.")
+  (add-to-list 'magic-mode-alist
+    '("\\(---\n\\)?AWSTemplateFormatVersion:" . cfn-yaml-mode))
+  (add-hook 'cfn-yaml-mode-hook 'flycheck-mode)
   )
+
+(provide '50-yaml)
+;;; 50-yaml ends here
