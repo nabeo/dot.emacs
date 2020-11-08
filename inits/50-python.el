3beaafe1e97d25ea4dd8eb3ade8c7468c28df426
nabeo <watanabe.michikazu@gmail.com>
2020-11-08 18:23:48 +0900

    add py-yapf

--- inits/50-python.el
+++ inits/50-python.el
@@ -33,5 +33,10 @@
    #'pipenv-projectile-after-switch-extended)
   )
 
+(use-package py-yapf
+  :ensure t
+  :hook (python-mode . py-yapf-enable-on-save)
+  )
+
 (provide '50-python)
 ;;; 50-python ends here
