94d141df58c9290adf90e4686e7138857761d0b5
Michikazu Watanabe <nabeop@hatena.ne.jp>
2021-03-05 10:55:02 +0900

    lsp-ui 向けの定義を独立させる

--- inits/30-lsp.el
+++ inits/30-lsp.el
@@ -47,52 +47,52 @@
     ("C-c r" . lsp-rename))
   :config
   (setq lsp-completion-provider :capf)
-  (use-package lsp-ui
-    :ensure t
-    :after lsp-mode
-    :custom
-    (scroll-margin 0)
-    ;; lsp-ui-doc
-    (lsp-ui-doc-enable t)
-    (lsp-ui-doc-header t)
-    (lsp-ui-doc-include-signature t)
-    (lsp-ui-doc-position 'bottom) ;; top, bottom, or at-point
-    (lsp-ui-doc-max-width 150)
-    (lsp-ui-doc-max-height 30)
-    (lsp-ui-doc-use-childframe nil)
-    (lsp-ui-doc-use-webkit nil)
-    (lsp-ui-doc-alignment 'window) ;; window or frame
-    (lsp-ui-doc-use-webkit (featurep 'xwidget-internal))
-    ;; lsp-ui-flycheck
-    (lsp-ui-flycheck-enable nil)
-    ;; lsp-ui-sideline
-    (lsp-ui-sideline-enable nil)
-    (lsp-ui-sideline-ignore-duplicate t)
-    (lsp-ui-sideline-show-symbol t)
-    (lsp-ui-sideline-show-hover t)
-    (lsp-ui-sideline-show-diagnostics nil)
-    (lsp-ui-sideline-show-code-actions nil)
-    ;; lsp-ui-imenu
-    (lsp-ui-imenu-enable nil)
-    (lsp-ui-imenu-kind-position 'top)
-    ;; lsp-ui-peek
-    (lsp-ui-peek-enable t)
-    (lsp-ui-peek-peek-height 20)
-    (lsp-ui-peek-list-width 50)
-    (lsp-ui-peek-fontify 'on-demand) ;; never, on-demand, or always
-    :hook
-    (lsp-mode . lsp-ui-mode)
-    :bind
-    (:map lsp-mode-map
-      ("C-c C-r" . lsp-ui-peek-find-references)
-      ("C-c C-j" . lsp-ui-peek-find-definitions)
-      ("C-c i" . lsp-ui-peek-find-implementation)
-      ("C-c m" . lsp-ui-imenu)
-      ("C-c s" . lsp-ui-sideline-mode)
-      ("C-c d" . lsp-ui-doc-show)
-      )
-    :config
+  )
+
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode
+  :custom
+  (scroll-margin 0)
+  ;; lsp-ui-doc
+  (lsp-ui-doc-enable t)
+  (lsp-ui-doc-header t)
+  (lsp-ui-doc-include-signature t)
+  (lsp-ui-doc-position 'bottom) ;; top, bottom, or at-point
+  (lsp-ui-doc-max-width 150)
+  (lsp-ui-doc-max-height 30)
+  (lsp-ui-doc-use-childframe nil)
+  (lsp-ui-doc-use-webkit (featurep 'xwidget-internal))
+  (lsp-ui-doc-alignment 'window) ;; window or frame
+  ;; lsp-ui-flycheck
+  (lsp-ui-flycheck-enable nil)
+  ;; lsp-ui-sideline
+  (lsp-ui-sideline-enable nil)
+  (lsp-ui-sideline-ignore-duplicate t)
+  (lsp-ui-sideline-show-symbol t)
+  (lsp-ui-sideline-show-hover t)
+  (lsp-ui-sideline-show-diagnostics nil)
+  (lsp-ui-sideline-show-code-actions nil)
+  ;; lsp-ui-imenu
+  (lsp-ui-imenu-enable nil)
+  (lsp-ui-imenu-kind-position 'top)
+  ;; lsp-ui-peek
+  (lsp-ui-peek-enable t)
+  (lsp-ui-peek-peek-height 20)
+  (lsp-ui-peek-list-width 50)
+  (lsp-ui-peek-fontify 'on-demand) ;; never, on-demand, or always
+  :hook
+  (lsp-mode . lsp-ui-mode)
+  :bind
+  (:map lsp-mode-map
+    ("C-c C-r" . lsp-ui-peek-find-references)
+    ("C-c C-j" . lsp-ui-peek-find-definitions)
+    ("C-c i" . lsp-ui-peek-find-implementation)
+    ("C-c m" . lsp-ui-imenu)
+    ("C-c s" . lsp-ui-sideline-mode)
+    ("C-c d" . lsp-ui-doc-show)
     )
+  :config
   )
 
 (use-package lsp-treemacs
