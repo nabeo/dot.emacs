1494cb9798a6ab8456d3ba7cf27445c7b9f51ff0
Michikazu Watanabe <nabeop@hatena.ne.jp>
2021-04-12 11:43:03 +0900

    50-direnv

--- inits/50-direnv.el
+++ inits/50-direnv.el
@@ -1,7 +1,15 @@
+;;; 50-direnv -- emacs-direnv
+;;; Commentary:
+
+;;; Code:
 (use-package direnv
   :ensure t
   :if (or (executable-find "direnv")
-          (file-executable-p (concat (getenv "GOPATH") "/bin/direnv")))
+        (file-executable-p (concat (getenv "GOPATH") "/bin/direnv")))
+  :custom
+  (direnv-always-show-summary t)
   :config
-  (direnv-mode)
-  (setq direnv-always-show-summary t))
+  (direnv-mode))
+
+(provide '50-direnv)
+;;; 50-direnv ends here
