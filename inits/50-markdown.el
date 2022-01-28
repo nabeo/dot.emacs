2e6d0853545f325e8a3e888effe0482d64830368
Michikazu Watanabe <nabeop@hatena.ne.jp>
2022-01-28 22:43:42 +0900

    50-markdown: use with outline-minor-mode

--- inits/50-markdown.el
+++ inits/50-markdown.el
@@ -1,3 +1,7 @@
+;;; 50-markdown --- markdown-mode
+;;; Commentary:
+
+;;; Code:
 (use-package markdown-mode
   :ensure t
   :commands (markdown-mode gfm-mode)
@@ -6,5 +10,9 @@
          ("\\.mdt" . markdown-mode))
   :init
   (if (executable-find "pandoc")
-      (setq markdown-command "pandoc"))
+    (setq markdown-command "pandoc"))
+  (add-hook 'markdown-mode-hook
+    #'(lambda () (outline-minor-mode t)))
   )
+
+;;; 50-markdown.el ends here
