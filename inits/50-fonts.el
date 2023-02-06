d88e80ed767ec38f313e3f2f987bf8e2545aee3b
nabeo <watanabe.michikazu@gmail.com>
2023-02-06 23:37:07 +0900

    50-fonts: use create-fontset-from-ascii-font

--- inits/50-fonts.el
+++ inits/50-fonts.el
@@ -1,45 +1,29 @@
-(let* ((size 12) ; ASCIIフォントのサイズ [9/10/12/14/15/17/19/20/...]
-       (asciifont "Monaco") ; ASCIIフォント
-       ;; (asciifont "Menlo") ; ASCIIフォント
-       ;; (asciifont "Ricty") ; ASCIIフォント
-       ;; (jpfont "TakaoExGothic")             ; 日本語フォント
-       ;; (jpfont "Hiragino Maru Gothic ProN") ; 日本語フォント
-       (jpfont "Hiragino Kaku Gothic ProN") ; 日本語フォント
-       ;; (jpfont "Hiragino Kaku Gothic") ; 日本語フォント
-       ;; (jpfont "Ricty")                     ; 日本語フォント
-       ;; (h (* size 10))
-       (fontspec (font-spec :family asciifont))
-       (jp-fontspec (font-spec :family jpfont :spacing 0)))
+(let* 
+  (
+    (asciifont "Monaco")
+    ;; (asciifont "Source Han Mono-12:weight=normal:slant=normal")
+    (jpfont "Hiragino Kaku Gothic ProN")
+    ;; (jpfont "Hiragino Maru Gothic ProN")
+    ;; (jpfont "Source Han Mono-12:weight=normal:slant=normal")
+    )
 
-  (set-fontset-font nil 'ascii fontspec)
-  ;; (set-face-attribute 'default nil :family asciifont :height h)
-  ;; http://sakito.jp/moin/moin.cgi/EmacsFont を参考にして必要そうなものを追加
-  (set-fontset-font nil 'jisx0201                 jp-fontspec)
-  (set-fontset-font nil 'latin-jisx0201           jp-fontspec)
-  (set-fontset-font nil 'japanese-jisx0208        jp-fontspec)
-  ;; (set-fontset-font nil 'japanese-jisx0208-1978   jp-fontspec)
-  ;; (set-fontset-font nil 'japanese-jisx0212        jp-fontspec)
-  ;; (set-fontset-font nil 'japanese-jisx0213-1      jp-fontspec)
-  ;; (set-fontset-font nil 'japanese-jisx0213-2      jp-fontspec)
-  ;; (set-fontset-font nil 'japanese-jisx0213-a      jp-fontspec)
-  (set-fontset-font nil 'japanese-jisx0213.2004-1 jp-fontspec)
-  (set-fontset-font nil 'katakana-sjis            jp-fontspec)
-  (set-fontset-font nil 'cp932-2-byte             jp-fontspec)
-  (set-fontset-font nil 'cp932                    jp-fontspec)
-  ;; (set-fontset-font nil '(#x3040 . #x309f)        jp-fontspec) ; ひらがな
-  ;; (set-fontset-font nil '(#x30a0 . #x30ff)        jp-fontspec) ; カタカナ
-  ;; (set-fontset-font nil '(#xff00 . #xffef)        jp-fontspec) ; 半角・全角系
-  ;; (set-fontset-font nil '(#x0370 . #x03FF)        fontspec)    ; ギリシャ文字 
-)
-;; フォントサイズの比を設定
-;; (setq face-font-rescale-alist
-;; 	'(("^-apple-hiragino.*" . 1.2)
-;; 	  (".*osaka-bold.*" . 1.2)
-;; 	  (".*osaka-medium.*" . 1.2)
-;; 	  (".*courier-bold-.*-mac-roman" . 1.0)
-;; 	  (".*monaco cy-bold-.*-mac-cyrillic" . 0.9)
-;; 	  (".*monaco-bold-.*-mac-roman" . 0.9)
-;; 	  ("-cdac$" . 1.3)))
+  ;; ASCII フォントをベースにしてフォントセットを作る
+  (create-fontset-from-ascii-font
+    asciifont
+    nil
+    "myfontset")
+
+  ;; unicode を丸ごと jpfont で設定する
+  (set-fontset-font
+    "fontset-myfontset"
+    'unicode
+    jpfont
+    nil
+    'append)
+
+  ;; 組み上げたフォントセットをデフォルトフレームに設定する
+  (add-to-list 'default-frame-alist '(font . "fontset-myfontset"))
+  )
 
 ;; カーソル位置のフォントを判別する
 ;; https://qiita.com/j8takagi/items/01aecdd28f87cdd3cd2c
