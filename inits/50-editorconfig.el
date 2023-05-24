54ad1f30851adab474778683a08ee4a94d8df2d2
nabeo <watanabe.michikazu@gmail.com>
2023-05-24 16:25:35 +0900

    50-editorconfig: fix flycheck warn

--- inits/50-editorconfig.el
+++ inits/50-editorconfig.el
@@ -1,4 +1,11 @@
+;;; 50-editorconfig --- editorconfig
+;;; Commentary:
+
+;;; Code:
 (use-package editorconfig
   :ensure t
   :config
   (editorconfig-mode 1))
+
+(provide '50-editorconfig)
+;;; 50-editorconfig.el ends here
