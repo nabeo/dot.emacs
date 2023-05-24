7a464f7d1032dd7c8036a90b0496f2105e0ac424
nabeo <watanabe.michikazu@gmail.com>
2023-05-24 16:26:43 +0900

    50-eshell: fix flycheck warn

--- inits/50-eshell.el
+++ inits/50-eshell.el
@@ -1,6 +1,13 @@
+;;; 50-eshell --- eshell-mode
+;;; Commentary:
+
+;;; Code:
 (use-package eshell
   :config
   (setq eshell-ask-to-save-last-dir 'always)
   (setq eshell-hist-ignoredups t)
   (setq eshell-cmpl-ignore-case t)
 )
+
+(provide '50-eshell)
+;;; 50-eshell.el ends here
