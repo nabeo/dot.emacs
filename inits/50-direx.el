f1fb38f5e3389cce6a9051c8dc673d4de67f3a2f
nabeo <watanabe.michikazu@gmail.com>
2023-05-24 16:19:51 +0900

    50-direx: fix flycheck warn

--- inits/50-direx.el
+++ inits/50-direx.el
@@ -1,3 +1,7 @@
+;;; 50-direx --- direx-mode
+;;; Commentary:
+
+;;; Code:
 ;; http://cx4a.blogspot.jp/2011/12/popwineldirexel.html
 ;; https://github.com/m2ym/direx-el.git
 (use-package direx
@@ -10,3 +14,6 @@
     (push '(direx:direx-mode :width 25 :dedicated t)
           popwin:special-display-config))
   )
+
+(provide '50-direx)
+;;; 50-direx.el ends here
