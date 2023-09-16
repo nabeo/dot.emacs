0df0c41d4b0ee5b25b4a48da3d3931bb0eacb6db
nabeo <watanabe.michikazu@gmail.com>
2023-09-16 22:25:52 +0900

    30-tree-sitter: treesit-auto-mode for terraform

--- inits/30-tree-sitter.el
+++ inits/30-tree-sitter.el
@@ -8,8 +8,6 @@
   :init
   ;; treesit-language-source-alist
   (add-to-list
-   'treesit-language-source-alist '(terraform . ("https://github.com/MichaHoffmann/tree-sitter-hcl" "main" "dialects/terraform/src")))
-  (add-to-list
    'treesit-language-source-alist '(hcl . ("https://github.com/MichaHoffmann/tree-sitter-hcl" "main" "src")))
   )
 
@@ -18,7 +16,17 @@
   :disabled (< emacs-major-version 29)
   :config
   (setq treesit-auto-install 'prompt)
-  (global-treesit-auto-mode))
+  (global-treesit-auto-mode)
+  (add-to-list 'treesit-auto-receipe-list
+               (make-treesit-auto-recipe
+                :lang 'terraform
+                :ts-mode 'terraform-ts-mode
+                :remap '(terraform-mode)
+                :url "https://github.com/MichaHoffmann/tree-sitter-hcl"
+                :revision "main"
+                :source-dir "dialects/terraform/src"
+                ))
+)
 
 (provide '30-tree-sitter)
 ;;; 30-tree-sitter.el ends here
