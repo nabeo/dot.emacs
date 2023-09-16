4ee621953edad705493c067e1959d93d14d8f3ae
nabeo <watanabe.michikazu@gmail.com>
2023-09-16 22:00:48 +0900

    30-eglot: add hooks

--- inits/30-eglot.el
+++ inits/30-eglot.el
@@ -2,6 +2,18 @@
 ;;; Commentary:
 
 ;;; Code:
+(use-package eglot
+  :hook
+  (prog-major-mode . eglot-ensure)
+  (go-mode . eglot-ensure)
+  (python-mode . eglot-ensure)
+  (typescript-mode . eglot-ensure)
+  (javascript-mode . eglot-ensure)
+  (rust-mode . eglot-ensure)
+  (json-mode . eglot-ensure)
+  (terraform-mode . eglot-ensure)
+  (yaml-mode . eglot-ensure)
+  )
 
 (provide '30-eglot)
 ;;; 30-eglot.el ends here
