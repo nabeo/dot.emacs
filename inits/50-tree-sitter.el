21484780c712c45c7b388df968312d0f017a71f6
nabeo <watanabe.michikazu@gmail.com>
2023-09-16 15:14:37 +0900

    50-tree-sitter: check emacs-major-version

--- inits/50-tree-sitter.el
+++ inits/50-tree-sitter.el
@@ -4,14 +4,15 @@
 ;;; Code:
 
 (use-package treesit
- :demand t
- :init
- ;; treesit-language-source-alist
- (add-to-list
-  'treesit-language-source-alist '(terraform . ("https://github.com/MichaHoffmann/tree-sitter-hcl" nil "dialects/terraform/src")))
- (add-to-list
-  'treesit-language-source-alist '(hcl . ("https://github.com/MichaHoffmann/tree-sitter-hcl" nil "src")))
- )
+  :demand t
+  :disabled (< emacs-major-version 29)
+  :init
+  ;; treesit-language-source-alist
+  (add-to-list
+   'treesit-language-source-alist '(terraform . ("https://github.com/MichaHoffmann/tree-sitter-hcl" nil "dialects/terraform/src")))
+  (add-to-list
+   'treesit-language-source-alist '(hcl . ("https://github.com/MichaHoffmann/tree-sitter-hcl" nil "src")))
+  )
 
 (provide '50-tree-sitter)
 ;;; 50-tree-sitter.el ends here
