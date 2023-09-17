1a48ec3e8cb7dc4da2f5cd77f4d70969d42ebb79
nabeo <watanabe.michikazu@gmail.com>
2023-09-17 11:28:48 +0900

    30-ivy: add ivy-xref

--- inits/30-ivy.el
+++ inits/30-ivy.el
@@ -61,6 +61,18 @@
     (ivy-read-action-function #'ivy-hydra-read-action))
   )
 
+(use-package ivy-xref
+  :ensure t
+  :init
+  ;; xref initialization is different in Emacs 27 - there are two different
+  ;; variables which can be set rather than just one
+  (when (>= emacs-major-version 27)
+    (setq xref-show-definitions-function #'ivy-xref-show-defs))
+  ;; Necessary in Emacs <27. In Emacs 27 it will affect all xref-based
+  ;; commands other than xref-find-definitions (e.g. project-find-regexp)
+  ;; as well
+  (setq xref-show-xrefs-function #'ivy-xref-show-xrefs))
+
 (use-package all-the-icons-ivy-rich
   :ensure t
   :disabled t
