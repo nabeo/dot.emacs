30bee57f534936d327d43b5722896d88bfbe6830
nabeo <watanabe.michikazu@gmail.com>
2023-09-30 18:32:00 +0900

    30-eglot: update

--- inits/30-eglot.el
+++ inits/30-eglot.el
@@ -5,15 +5,14 @@
 (use-package eglot
   :commands (eglot-ensure)
   :hook
-  (prog-major-mode . eglot-ensure)
-  (go-mode . eglot-ensure)
-  (python-mode . eglot-ensure)
+  (go-mode . eglot-ensure)              ; brew install gopls
+  (python-mode . eglot-ensure)          ; brew install python-lsp-server
   (typescript-mode . eglot-ensure)      ; npm i -g typescript-language-server
   (javascript-mode . eglot-ensure)
   (rust-mode . eglot-ensure)
   (json-mode . eglot-ensure)            ; npm i -g vscode-json-languageserver
-  (terraform-mode . eglot-ensure)
-  (yaml-mode . eglot-ensure)
+  (terraform-mode . eglot-ensure)       ; brew install terraform-ls
+  (yaml-mode . eglot-ensure)            ; brew install yaml-language-server
   :config
   ;; for terraform-ls (brew install terraform-ls)
   (add-to-list
