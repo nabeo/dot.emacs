caee116caf0df3c6dedd170e5cc51643cd1513ad
nabeo <watanabe.michikazu@gmail.com>
2023-10-08 16:37:34 +0900

    30-vertico: load embark-consult

--- inits/30-vertico.el
+++ inits/30-vertico.el
@@ -237,6 +237,7 @@
 ;; Consult users will also want the embark-consult package.
 (use-package embark-consult
   :ensure t ; only need to install it, embark loads it after consult if found
+  :after (consult embark)
   :hook
   (embark-collect-mode . consult-preview-at-point-mode))
 
