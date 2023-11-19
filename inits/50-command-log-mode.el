f77a381c8cf999303b22e9af2c9bf88dba20f7c0
nabeo <watanabe.michikazu@gmail.com>
2023-11-19 22:42:08 +0900

    50-command-log-mode: add command-log-mode

---
+++ inits/50-command-log-mode.el
@@ -0,0 +1,10 @@
+;;; 50-command-log-mode --- command-log-mode
+;;; Commentary:
+
+;;; Code:
+(use-package command-log-mode
+  :ensure t
+  )
+
+(provide '50-command-log-mode)
+;;; 50-command-log-mode.el ends here
