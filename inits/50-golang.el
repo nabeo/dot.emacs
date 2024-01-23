d642f8e87de15c23d266296430a07a6a3c39a55d
nabeo <watanabe.michikazu@gmail.com>
2024-01-23 17:13:26 +0900

    50-golang: cleanup

--- inits/50-golang.el
+++ inits/50-golang.el
@@ -11,36 +11,13 @@
   (let ((my/gopath-env (getenv "GOPATH")))
     (if (and (eq my/gopath-env nil) (file-exists-p "~/Development/golang"))
       (setenv "GOPATH" (expand-file-name "~/Development/golang"))))
-
-  ;; set PATH
-  (cond ((file-exists-p (concat (getenv "GOPATH") "/bin"))
-         (let ((my/gopath-bin (expand-file-name "bin" (getenv "GOPATH"))))
-           (unless (member my/gopath-bin exec-path)
-             (setq exec-path (cons my/gopath-bin
-                                   exec-path))
-             (setenv "PATH" (concat (getenv "PATH") ":" my/gopath-bin))))))
-
   (if (eq (member (expand-file-name "~") (s-split ":" (getenv "GOPATH"))) nil)
-    (setenv "GOPATH" (concat (getenv "GOPATH") ":" (expand-file-name "~"))))
+      (setenv "GOPATH" (concat (getenv "GOPATH") ":" (expand-file-name "~"))))
 
   :hook
   (( before-save . gofmt-before-save ))
 
   :config
-  ;; set commands
-  (cond ((file-executable-p "/opt/local/bin/go")
-         (setq go-command "/opt/local/bin/go")
-          (setq godoc-command "/opt/local/bin/go doc")))
-  (cond
-    ;; go install golang.org/x/tools/cmd/goimports@latest
-    ((executable-find "goimports")
-      (setq gofmt-command "goimports")))
-  ;; go install github.com/rogpeppe/godef@latest
-  (cond ((file-executable-p (concat (getenv "GOPATH") "/bin/godef"))
-         (setq godef-command (concat (getenv "GOPATH") "/bin/godef"))))
-
-  (add-hook 'go-mode-hook (lambda ()
-                            (local-set-key (kbd "M-.") 'godef-jump)))
   )
 
 (use-package go-snippets
