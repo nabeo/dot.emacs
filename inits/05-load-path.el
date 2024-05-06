2a5d841a25b1e3a3b4253fec8e073643069c3d84
nabeo <watanabe.michikazu@gmail.com>
2024-05-06 10:13:17 +0900

    50-load-path: fix error

--- inits/05-load-path.el
+++ inits/05-load-path.el
@@ -4,10 +4,11 @@
 ;;; Code:
 ;; ~/local/emacs/site-lisp以下は再帰的にload-pathに追加する
 (let ((default-directory (expand-file-name "~/local/emacs/site-lisp")))
-  (if (file-directory-p default-directory)
-    (add-to-list 'load-path default-directory)
-    (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-      (normal-top-level-add-subdirs-to-load-path))))
+  (if (file-exists-p default-directory)
+      (progn
+        (add-to-list 'load-path default-directory)
+        (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+            (normal-top-level-add-subdirs-to-load-path)))))
 
 (provide '05-load-path)
 ;;; 05-load-path.el ends here
