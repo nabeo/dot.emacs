8876f8e49920c2baea643966f4421d3db1ff61a7
nabeo <watanabe.michikazu@gmail.com>
2024-05-29 12:03:04 +0900

    50-docker: auto-mode-alist for docker-compose-mode

--- inits/50-docker.el
+++ inits/50-docker.el
@@ -16,10 +16,16 @@
   ("Dockerfile.*\\'" . dockerfile-mode)
   :init
   :config
-)
+  )
 
 (use-package docker-compose-mode
-  :ensure t)
+  :ensure t
+  :mode
+  ("docker-compose\\.yaml" . docker-compose-mode)
+  ("docker-compose\\.yml" . docker-compose-mode)
+  ("compose\\.yaml" . docker-compose-mode)
+  ("compose\\.yml" . docker-compose-mode)
+  )
 
 (provide '50-docker)
 ;;; 50-docker.el ends here
