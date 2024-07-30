241903debdd38a290ae62434fcac943efa6eaf67
nabeo <watanabe.michikazu@gmail.com>
2024-07-30 14:26:15 +0900

    50-fonts: add UDEV Gothic NF

--- inits/50-fonts.el
+++ inits/50-fonts.el
@@ -19,7 +19,11 @@
             "Cica"
             ;; M PLUS 1 Code
             ;; https://mplusfonts.github.io/
-            "M PLUS 1 Code Medium"))
+            "M PLUS 1 Code Medium"
+            ;; UDEV Gothic
+            ;; https://github.com/yuru7/udev-gothic
+            "UDEV Gothic NF"
+            ))
          (my/fontfamily (elt my/fontfamilies (abs (% (random t) (length my/fontfamilies))))))
     (if (find-font (font-spec :name my/fontfamily))
         (set-face-attribute 'default nil :family my/fontfamily :height 140)
