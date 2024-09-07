f89666eae1c829ecac4eab6c314798dc120cf8bb
nabeo <watanabe.michikazu@gmail.com>
2024-09-07 17:11:13 +0900

    50-git: use diff-hl, instead of git-gutter+

--- inits/50-git.el
+++ inits/50-git.el
@@ -24,31 +24,13 @@
   (forge-database-file (expand-file-name "data/forge-database.sqlite" user-emacs-directory))
   )
 
-(use-package git-gutter+
+(use-package diff-hl
   :ensure t
   :bind
-  ((:map git-gutter+-mode-map
-         ;; Jump between hunks
-         ("M-n" . git-gutter+-next-hunk)
-         ("M-p" . git-gutter+-previous-hunk)
-         ;; Act on hunks
-         ("C-x v =" . git-gutter+-show-hunk)
-         ("C-x r" . git-gutter+-revert-hunks)
-         ;; Stage hunk at point.
-         ;; If region is active, stage all hunk lines within the region.
-         ("C-x T" . git-gutter+-stage-hunks)
-         ("C-x c" . git-gutter+-commit)
-         ("C-x C" . git-gutter+-stage-and-commit)
-         ("C-x C-y" . git-gutter+-stage-and-commit-whole-buffer)
-         ("C-x U" . git-gutter+-unstage-whole-buffer)
-         ))
-  :init
-  (add-hook 'prog-mode-hook 'git-gutter+-mode)
-  (add-hook 'yaml-mode-hook 'git-gutter+-mode)
-  (add-hook 'emacs-lisp-mode-hook 'git-gutter+-mode)
-  (add-hook 'go-mode-hook 'git-gutter+-mode)
+  ("M-n" . diff-hl-next-hunk)
+  ("M-p" . diff-hl-previous-hunk)
   :config
-  )
+  (global-diff-hl-mode))
 
 (use-package git-link
   :ensure t
