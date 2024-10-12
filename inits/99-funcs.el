ab3aa80929251e69f3b5ad35fd0aca93f84ecd2e
nabeo <watanabe.michikazu@gmail.com>
2024-10-12 11:39:39 +0900

    add nabeo/randomly-select-theme

--- inits/99-funcs.el
+++ inits/99-funcs.el
@@ -2,6 +2,12 @@
 ;;; Commentary:
 
 ;;; Code:
+(defun nabeo/randomly-select-theme ()
+  "randomly select doom-theme and font"
+  (interactive)
+  (nabeo/randomly-select-doom-theme)
+  (nabeo/set-my-font)
+  )
 
 (provide '99-funcs)
 ;;; 99-funcs.el ends here
