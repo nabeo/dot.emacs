e1c97ebc28364b7a8759dece4f18fa3c51540b0b
nabeo <watanabe.michikazu@gmail.com>
2025-02-04 19:49:44 +0900

    50-postgresql: PostgreSQL クライアント

---
+++ inits/50-postgresql.el
@@ -0,0 +1,12 @@
+;;; 50-postgresql --- PostgreSQL
+;;; Commentary:
+
+;;; Code:
+(use-package pg
+  :vc (:fetcher github :repo "emarsden/pg-el"))
+
+(use-package pgmacs
+  :vc (:fetcher github :repo "emarsden/pgmacs"))
+
+(provide '50-postgresql)
+;;; 50-postgresql.el ends here
