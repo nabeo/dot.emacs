ff69bc2e5e0b370491b23638760d27bd8b35fd9c
nabeo <watanabe.michikazu@gmail.com>
2025-02-24 17:55:40 +0900

    99-misc: add request and shell-maker from melpa

---
+++ inits/99-misc.el
@@ -0,0 +1,14 @@
+;;; 99-misc --- misc packages
+;;; Commentary:
+
+;;; Code:
+(use-package request
+  :ensure t
+  :pin melpa)
+
+(use-package shell-maker
+  :ensure t
+  :pin melpa)
+
+(provide '99-misc)
+;;; 99-misc.el ends here
