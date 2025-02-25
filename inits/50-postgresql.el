2d0d1b763f221fc9983ff27297d70b4be53cf8f5
nabeo <watanabe.michikazu@gmail.com>
2025-02-25 12:36:37 +0900

    50-postgresql: Emacs 30 では標準の use-package に組み込まれた :vc を使用する

--- inits/50-postgresql.el
+++ inits/50-postgresql.el
@@ -2,11 +2,23 @@
 ;;; Commentary:
 
 ;;; Code:
-(use-package pg
-  :vc (:fetcher github :repo "emarsden/pg-el"))
 
-(use-package pgmacs
-  :vc (:fetcher github :repo "emarsden/pgmacs"))
+(cond
+  ;; Emacs 30 以降では use-package に組み込まれている
+  ((<= emacs-major-version 30)
+    (use-package pg
+      :ensure t
+      :vc (pg :url "https://github.com/emarsden/pg-el" :branch "main"))
+    (use-package pgmacs
+      :ensure t
+      :vc (pgmacs :url "https://github.com/emarsden/pgmacs" :branch "main")))
+  ;; Emacs 30 以前の環境では vc-use-package を利用する
+  ((package-installed-p 'vc-use-package)
+    (use-package pg
+      :vc (pg :type git :host github :repo "emarsden/pg-el"))
+    (use-package pgmacs
+      :vc (pgmacs :type git :host github :repo "emarsden/pgmacs"))
+    ))
 
 (provide '50-postgresql)
 ;;; 50-postgresql.el ends here
