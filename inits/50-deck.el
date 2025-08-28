74adcfd7e4f88ba2926249562ffb83995f79d045
nabeo <watanabe.michikazu@gmail.com>
2025-08-28 10:11:22 +0900

    50-deck: add deck-slides.el

---
+++ inits/50-deck.el
@@ -0,0 +1,13 @@
+;;; 50-deck --- k1Low/deck
+;;; Commentary:
+
+;;; Code:
+(use-package deck-slides
+  :ensure t
+  :vc (deck-slides
+        :url "https://github.com/zonuexe/deck-slides.el"
+        :branch "mster")
+  )
+
+(provide '50-deck)
+;;; 50-deck.el ends here
--- my-customize.el
+++ my-customize.el
@@ -7,17 +7,17 @@
     '(bbdb bbdb-vcard chezmoi combobulate command-log-mode company-box
        company-quickhelp consult-company consult-eglot consult-ghq
        consult-git-log-grep consult-ls-git consult-projectile copilot
-       copilot-chat cp5022x diff-hl direx docker docker-compose-mode
-       dockerfile-mode doom-modeline doom-themes editorconfig ellama
-       embark-consult envrc flycheck-eglot flycheck-inline
-       flycheck-kotlin forge git-link gnu-elpa-keyring-update
-       go-projectile graphql-ts-mode groovy-mode howm
-       ibuffer-projectile id-manager init-loader json-mode
-       jsonnet-mode k8s-mode kotlin-mode kotlin-ts-mode kubedoc kubel
-       lua-mode magit-find-file magit-popup magit-svn marginalia
-       mermaid-mode migemo mise nerd-icons-completion orderless pass
-       pg pgmacs pinentry podcaster poly-ansible poly-erb
-       poly-markdown poly-rst poly-ruby popwin
+       copilot-chat cp5022x deck-slides diff-hl direx docker
+       docker-compose-mode dockerfile-mode doom-modeline doom-themes
+       editorconfig ellama embark-consult envrc flycheck-eglot
+       flycheck-inline flycheck-kotlin forge git-link
+       gnu-elpa-keyring-update go-projectile graphql-ts-mode
+       groovy-mode howm ibuffer-projectile id-manager init-loader
+       json-mode jsonnet-mode k8s-mode kotlin-mode kotlin-ts-mode
+       kubedoc kubel lua-mode magit-find-file magit-popup magit-svn
+       marginalia mermaid-mode migemo mise nerd-icons-completion
+       orderless pass pg pgmacs pinentry podcaster poly-ansible
+       poly-erb poly-markdown poly-rst poly-ruby popwin
        projectile-git-autofetch protobuf-mode puni py-yapf
        rainbow-delimiters review-mode ruby-electric rust-mode shimbun
        terraform-mode treemacs-icons-dired treemacs-magit
@@ -25,7 +25,10 @@
        treesit-auto treesit-fold typescript-mode vertico vterm-toggle
        wanderlust web-mode yafolding yasnippet-snippets))
  '(package-vc-selected-packages
-    '((pgmacs :url "https://github.com/emarsden/pgmacs" :branch "main")
+    '((deck-slides :url "https://github.com/zonuexe/deck-slides.el"
+        :branch "mster")
+       (pgmacs :url "https://github.com/emarsden/pgmacs" :branch
+         "main")
        (pg :url "https://github.com/emarsden/pg-el" :branch "main")
        (combobulate :url "https://github.com/mickeynp/combobulate"
          :branch "master")))
