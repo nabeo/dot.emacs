7514827bb15b5f5cc4be208c2042fcf5b1ec77d5
nabeo <watanabe.michikazu@gmail.com>
2025-10-10 12:17:35 +0900

    50-flycheck: textlint 向けの設定を追加

--- inits/50-flycheck.el
+++ inits/50-flycheck.el
@@ -25,6 +25,30 @@ See `https://github.com/aws-cloudformation/cfn-python-lint'."
                         (id "E" (one-or-more digit)) ":" (message) line-end))
     :modes (cfn-json-mode cfn-yaml-mode))
   (add-to-list 'flycheck-checkers 'cfn-lint)
+
+  ;; textlint
+  ;; flycheck-textlint-executable の設定
+  ;; パスに textlint があればそれを使い、なければ mise 経由で探す
+  (cond
+    ((executable-find "textlint")
+      (setq flycheck-textlint-executable "textlint"))
+    ((executable-find "mise")
+      (setq flycheck-textlint-executable
+        (string-trim
+          (shell-command-to-string
+            (mapconcat #'shell-quote-argument (list "mise" "which" "textlint") " ")))))
+    (t "textlint"))
+  ;; Emacs 向けの textlintrc を指定する
+  (setq flycheck-textlint-config (concat user-emacs-directory "data/textlintrc.json"))
+  ;; https://github.com/textlint-ja/textlint-rule-preset-ja-technical-writing
+  (add-to-list 'flycheck-textlint-plugin-alist
+    '(t . "textlint-rule-preset-ja-technical-writing"))
+  ;; https://github.com/textlint-ja/textlint-rule-ja-unnatural-alphabet
+  (add-to-list 'flycheck-textlint-plugin-alist
+    '(t . "textlint-rule-ja-unnatural-alphabet"))
+  ;; https://github.com/textlint-ja/textlint-rule-ja-no-redundant-expression
+  (add-to-list 'flycheck-textlint-plugin-alist
+    '(t . "textlint-rule-ja-no-redundant-expression") )
   )
 
 (use-package flycheck-inline
