f2aa77ec0128eed53f1e0f64387af982725a47ad
nabeo <watanabe.michikazu@gmail.com>
2025-10-10 13:01:17 +0900

    50-review: textlint を有効にする

--- inits/50-review.el
+++ inits/50-review.el
@@ -3,7 +3,12 @@
 
 ;;; Code:
 (use-package review-mode
-  :ensure t)
+  :ensure t
+  :hook
+  (review-mode .
+    (lambda ()
+      (flycheck-add-mode 'textlint 'review-mode)))
+  )
 
 (provide '50-review)
 ;;; 50-review.el ends here
