04402e90e5635c57a71e638008a1e4149382145a
nabeo <watanabe.michikazu@gmail.com>
2025-10-10 12:18:06 +0900

    50-kotlin: kotlin で flycheck を使用する

--- inits/50-kotlin.el
+++ inits/50-kotlin.el
@@ -15,6 +15,13 @@
   :init
   (eval-after-load 'flycheck
     '(flycheck-kotlin-setup))
+  :config
+  (add-hook 'kotlin-ts-mode-hook
+    '(lambda ()
+       (add-to-list 'flycheck-disabled-checkers 'lsp)
+       (flycheck-add-mode 'kotlin-ktlint 'kotlin-ts-mode)
+       (flycheck-add-mode 'kotlin-ktlint 'kotlin-mode)
+       ))
   )
 
 ;;; 50-kotlin.el ends here
