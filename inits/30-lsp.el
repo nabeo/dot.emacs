13d7113a01d3dc8471143557761591d5a0cdd06d
nabeo <watanabe.michikazu@gmail.com>
2026-05-18 12:41:43 +0900

    30-lsp: kotlin-lsp は v262.4739.0 から intellij-server を使うようになった

--- inits/30-lsp.el
+++ inits/30-lsp.el
@@ -80,9 +80,11 @@
 
   ;; kotlin-lsp
   ;; https://github.com/Kotlin/kotlin-lsp/blob/main/scripts/lsp-kotlin-emacs-lsp-mode.el
+  (setq kotlin-lsp-bin
+    (concat my/homebrew-prefix "/Caskroom/kotlin-lsp/262.4739.0/kotlin-server-262.4739.0/bin/intellij-server"))
   (defun kotlin-lsp-server-fun ()
-    (list "kotlin-lsp" "--stdio"))
-  (cond ((executable-find "kotlin-lsp")
+    (list kotlin-lsp-bin "--stdio"))
+  (cond ((executable-find kotlin-lsp-bin)
           (add-to-list 'lsp-language-id-configuration '(kotlin-mode . "kotlin-lsp"))
           (add-to-list 'lsp-language-id-configuration '(kotlin-ts-mode . "kotlin-lsp"))
           (lsp-register-client
